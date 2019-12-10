5f6b42e48b405ce0d3f57ed13fd2a9837cb88bb2
HaoSususudio <bluehao85@gmail.com>
2019-12-10 13:10:29 -0500

    photo bundle fectching somewhat works

--- Poof-O-Floof-ui/src/app/main-photo/main-photo.component.ts
+++ Poof-O-Floof-ui/src/app/main-photo/main-photo.component.ts
@@ -5,6 +5,7 @@ import { PhotoStreamMetaData, UserIpLocInfo } from '../services/models.service';
 import { ReplaySubject, BehaviorSubject } from 'rxjs';
 import { ConditionalExpr } from '@angular/compiler';
 import { LocationService } from '../services/location.service';
+import { AnimalPhotoJSON } from '../services/models.service';
 
 @Component({
   selector: 'app-main-photo',
@@ -13,11 +14,16 @@ import { LocationService } from '../services/location.service';
 })
 
 export class MainPhotoComponent implements OnInit {
-  private psIndexArray: Array<number>;
+  private photoStream: AnimalPhotoJSON[];
+  private psIndexArray: number[];
+  private psCurrentState: PhotoStreamMetaData;
   private photoDisplayIndex = 0;
   private photoStreamIndex = 0;
   mainFramePhotoUrl: string;
   mainFramePhotoType: string;
+  totalPhotoNumber: number;
+  lastBundleSize: number;
+  private PHOTO_RESERVE_SIZE = 5;
   private LARGE_URL_SUFFIX = '&width=600';
 
   constructor(
@@ -26,42 +32,53 @@ export class MainPhotoComponent implements OnInit {
   ) {
     const psArraySize = this.photoUrlProvider.getMaxPhotoStreamSize();
     this.psIndexArray = [...Array(psArraySize).keys()];
+    this.photoStream = [];
+    this.psCurrentState = new PhotoStreamMetaData();
   }
 
   ngOnInit() {
-    this.addMorePhotos();
-    this.setMainFramePhotoUrl();
+    console.log('Echo from ngOnInit');
+    this.photoUrlProvider.signUpPhotoBundle().subscribe(
+      pd => {
+        if (pd && pd.length) {
+          console.log(pd);
+          // this.photoStream.concat(pd);
+          this.photoStream = this.photoStream.concat(pd);
+        }
+      });
   }
 
   addMorePhotos() {
-    this.locService.getUserIpLocInfo()
-      .subscribe(
-        ipLoc => {
-          this.photoUrlProvider.requestANewPhotoBundle(ipLoc)
-            .subscribe(
-              pscs => {
-                if (pscs !== undefined) {
-                  // this.psCurrentState = pscs;
-                  this.shuffle(this.psIndexArray, pscs.totPhotoNum - pscs.lastBundleSize, pscs.totPhotoNum - 1);
-                }
-              });
-        });
+    console.log('in component, size: ' + this.photoUrlProvider.requestANewPhotoBundle());
+    // .subscribe(
+    //   pscs => {
+    //     if (pscs) {
+    //       this.totalPhotoNumber = pscs.totPhotoNum;
+    //       this.lastBundleSize = pscs.totPhotoNum;
+    //       const shuffleStart = pscs.totPhotoNum - pscs.lastBundleSize;
+    //       const shuffleEnd = pscs.totPhotoNum - 1;
+    //       this.shuffle(this.psIndexArray, shuffleStart, shuffleEnd);
+    //       console.log(this.psIndexArray);
+    //     }
+    //   });
+  }
+
+  printPS() {
+    console.log(this.photoStream);
   }
 
   nextRandomPhoto() {
-    this.photoDisplayIndex += 1;
     this.setMainFramePhotoUrl();
+    this.photoDisplayIndex += 1;
+    if (this.photoDisplayIndex < this.totalPhotoNumber - this.PHOTO_RESERVE_SIZE) {
+      this.addMorePhotos();
+    }
   }
 
   setMainFramePhotoUrl() {
-    this.photoUrlProvider.getPhotoStream()
-      .subscribe(
-        data => {
-          this.photoStreamIndex = this.psIndexArray[this.photoDisplayIndex];
-          this.mainFramePhotoUrl = data[this.photoStreamIndex].fullUrl + this.LARGE_URL_SUFFIX;
-          this.mainFramePhotoType = data[this.photoStreamIndex].type;
-        }
-      );
+    this.photoStreamIndex = this.psIndexArray[this.photoDisplayIndex];
+    this.mainFramePhotoUrl = this.photoStream[this.photoStreamIndex].fullUrl + this.LARGE_URL_SUFFIX;
+    this.mainFramePhotoType = this.photoStream[this.photoStreamIndex].type;
   }
 
   /**
--- Poof-O-Floof-ui/src/app/services/photo-url-provider.service.ts
+++ Poof-O-Floof-ui/src/app/services/photo-url-provider.service.ts
@@ -3,17 +3,19 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable, BehaviorSubject, ReplaySubject, AsyncSubject } from 'rxjs';
 import { UserIpLocInfo, PhotoStreamMetaData } from '../services/models.service';
 import { LocationService } from '../services/location.service';
+import { AnimalPhotoJSON } from '../services/models.service';
 
 @Injectable({ providedIn: 'root' })
 
 export class PhotoUrlProviderService {
   // private TEST_PHOTO_BUNDLE_URL = 'https://jsonplaceholder.typicode.com/photos?albumId=1';
   private PHOTO_BUNDLE_URL = 'http://localhost:8080/Poof-O-Floof/api/location';
-  private MAX_PHOTO_STREAM_SIZE = 500;
+  private MAX_PHOTO_STREAM_SIZE = 200;
   private photoBundleSize: number;
   private userIpLocInfo: UserIpLocInfo;
 
   private photoStream$: ReplaySubject<AnimalPhotoJSON>;
+  private photoBundle$: BehaviorSubject<Array<AnimalPhotoJSON>>;
   private psCurrentState: PhotoStreamMetaData;
   private psCurrentState$: BehaviorSubject<PhotoStreamMetaData>;
 
@@ -21,48 +23,50 @@ export class PhotoUrlProviderService {
     private http: HttpClient,
     private locService: LocationService
   ) {
-    this.locService.getUserIpLocInfo();
-    this.photoStream$ = new ReplaySubject<AnimalPhotoJSON>(this.MAX_PHOTO_STREAM_SIZE);
+    this.locService.getUserIpLocInfo().subscribe(ipLoc => this.userIpLocInfo = ipLoc);
+    // this.photoStream$ = new ReplaySubject<Array<AnimalPhotoJSON>>(this.MAX_PHOTO_STREAM_SIZE);
+    this.photoBundle$ = new BehaviorSubject<Array<AnimalPhotoJSON>>(undefined);
     this.psCurrentState = new PhotoStreamMetaData();
     this.psCurrentState.totPhotoNum = 0;
     this.psCurrentState$ = new BehaviorSubject<PhotoStreamMetaData>(undefined);
+
+
   }
 
   getMaxPhotoStreamSize() {
     return this.MAX_PHOTO_STREAM_SIZE;
   }
 
-  requestANewPhotoBundle(ipLoc: UserIpLocInfo): Observable<PhotoStreamMetaData> {
-    this.http.post<AnimalPhotoJSON>(this.PHOTO_BUNDLE_URL, JSON.stringify(ipLoc))
+  requestANewPhotoBundle(): number {
+    this.http.post<Array<AnimalPhotoJSON>>(this.PHOTO_BUNDLE_URL, JSON.stringify(this.userIpLocInfo))
       .subscribe(
-        data => {
-          this.psCurrentState.lastBundleSize = Object.keys(data).length;
-          this.psCurrentState.totPhotoNum += this.psCurrentState.lastBundleSize;
-          this.psCurrentState$.next(this.psCurrentState);
-          this.photoStream$.next(data);
+        photoBundle => {
+          const size = Object.keys(photoBundle).length;
+          console.log('Got photo bundle gotten from Tomcat');
+          console.log('Size: ' + Object.keys(photoBundle).length);
+          this.photoBundle$.next(photoBundle);
+          return size;
         },
         error => {
           console.log('User location is not ready. Tomcat is mad.');
           // console.error(error.error);
         }
       );
-    return this.psCurrentState$.asObservable();
+    return undefined;
   }
 
   getPhotoStream(): Observable<AnimalPhotoJSON> {
     return this.photoStream$.asObservable();
   }
 
-  getPhotoStreamCurrentState(): Observable<PhotoStreamMetaData> {
+  getPSCurrentState(): Observable<PhotoStreamMetaData> {
     return this.psCurrentState$.asObservable();
   }
-}
 
-export interface AnimalPhotoJSON {
-  animalId: number;
-  photoId: number;
-  type: string;
-  fullUrl: string;
+  signUpPhotoBundle(): Observable<Array<AnimalPhotoJSON>> {
+    return this.photoBundle$.asObservable();
+  }
+
 }
 
 export interface TestPhotoJSON {
